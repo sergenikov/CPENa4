5b36990a5b477763efe8b7482c7d7262a0a3127c
sergenikov <sg.skov@gmail.com>
2015-11-01 11:32:29 -0800

    Added comments for clarity.

--- q4crack.py
+++ q4crack.py
@@ -2,6 +2,15 @@ import sys
 import hashlib
 import binascii
 
+"""
+Replaces the password located in the binary file with the
+newly calculated hash.
+Usage:
+python q4crack.py hello
+Where hello is the new password.
+Any other usage is undefined and unpredictable.
+"""
+
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 
@@ -9,7 +18,7 @@ new_pwd = sys.argv[1]
 print "new_password is", new_pwd
 
 new_pwd_sha1 = hashlib.sha1(new_pwd) # hashed password as byte array
-print "hello hash", binascii.hexlify(new_pwd_sha1.digest())
+print "New hash", binascii.hexlify(new_pwd_sha1.digest())
 
 list_sha1 = list(new_pwd_sha1.hexdigest())
 print "LIST SHA1"
@@ -30,7 +39,8 @@ with binary:
     decimal = int(hexadecimal, 16)
     binary = bin(decimal)[2:].zfill(8)
     print("hex: %s" % (hexadecimal))
-	
+
+# Open file again to write the new password in place of the old one.
 binary = open('35643097.program2.exe', 'r+b')
 binary.seek(75804)
 binary.write(sha1_bytes)
